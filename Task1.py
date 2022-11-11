15f5fd0df2452156c42086547d58f411fae7463f
Konstantin <klik@mail.ru>
2022-11-11 14:49:26 +0300

    6 семинар

--- Task1.py
+++ Task1.py
@@ -4,14 +4,12 @@
 
 from random import randint
 
-N = 5
+N = 7
 
 listOld = []
 listNew = []
 
-for i in range(N):
-    listOld.append(randint(1, 20))
-    if i % 2 != 0:
-        listNew.append(listOld[i])
+listOld = [(lambda i: randint(1, 20)) (i) for i in range(N)]
+listNew = [j for j in listOld if (listOld.index(j) + 1) % 2 == 0]
 
 print(f'{listOld} -> на нечётных позициях элементы {" и " .join(map(str, listNew))}, их сумма: {sum(listNew)}')
